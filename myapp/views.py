3f852b5f22e9b25ad88e254cd6de147b1a552d6d
mdkawsar <info.mdkawsardev@gmail.com>
2026-03-06 22:21:30 +0600

    Some changes

--- myapp/views.py
+++ myapp/views.py
@@ -25,6 +25,3 @@ def userlogin(request):
         else:
             return render(request, 'login.html')
     return render(request, 'login.html')
-def logoutuser(request):
-    logout(request)
-    return redirect('/login')
